525e0ca2a4e2e3a06672e41369066534535bf59c
Naman Gupta <naman13064@iiitd.ac.in>
2014-02-15 01:53:41 +0530

    Second..nothing much

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-# CSRF_ENABLED = True
-# SECRET_KEY = 'naman'
+CSRF_ENABLED = True
+SECRET_KEY = 'naman'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' }]
